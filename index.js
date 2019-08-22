2c06e3fdb747819216a1614f6ea9772d5e878e7d
eric.chiou <eric.chiou@acer.com>
2019-08-22 17:16:39 +0800

    add menu UI

--- index.js
+++ index.js
@@ -4,12 +4,46 @@ const componentList = [
   'image-editor'
 ]
 
-let showMenu = false;
-
 window.onload = () => {
   selectComponent(0);
+
+  window.addEventListener('resize', onResize);
 };
 
 function selectComponent(index) {
   document.getElementById('component-iframe').src = `./${componentList[index]}/${componentList[index]}.html`;
+
+  if (window.innerWidth < 768) {
+    closeMenu();
+  }
+}
+
+function onResize() {
+  const menu = document.getElementsByClassName('component-list')[0];
+  if (!menu) { return; }
+
+  menu.style.left = (window.innerWidth < 768) ? '-300px' : '0px';
+}
+
+function menuOnClick() {
+  const menu = document.getElementsByClassName('component-list')[0];
+  if (!menu) { return; }
+
+  menu.style.left = (menu.style.left === '0px') ? '-300px' : '0px';
+  maskHandler(menu);
+}
+
+function maskHandler(menu) {
+  const mask = document.getElementById('mask');
+  if (!mask) { return; }
+
+  mask.style.display = (menu.style.left === '0px') ? 'initial' : 'none';
+}
+
+function closeMenu() {
+  const menu = document.getElementsByClassName('component-list')[0];
+  if (!menu) { return; }
+
+  menu.style.left = '-300px';
+  maskHandler(menu);
 }
